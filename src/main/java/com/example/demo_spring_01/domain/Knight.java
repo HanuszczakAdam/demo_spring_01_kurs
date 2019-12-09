a6fdfa46e4099fd61512f127748b3357d78826bc
Adam Hanuszczak Acer <hanyhusajn@gmail.com>
2019-12-09 13:16:52 +0100

    Wstrzykiwanie kolekcji

--- src/main/java/com/example/demo_spring_01/domain/Knight.java
+++ src/main/java/com/example/demo_spring_01/domain/Knight.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
 public class Knight {
 
     private String name ;
@@ -24,6 +26,19 @@ public class Knight {
     public void setQuest(Quest quest) {
         System.out.println("Ustawiam zadanie dla rycerza");
         this.quest = quest;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Knight knight = (Knight) o;
+        return age == knight.age;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age);
     }
 
     public void setAge(int age) {
--- src/main/java/com/example/demo_spring_01/domain/Tournament.java
+++ src/main/java/com/example/demo_spring_01/domain/Tournament.java
@@ -4,25 +4,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Component
 public class Tournament {
 
     @Autowired
-    @Qualifier(value="percival")
-    private Knight knight;
+//    private Map<String, Knight> knights;
+    private List<Knight> knights;
 
     public Tournament() {}
 
     public void duel() {
-        knight.setAge(knight.getAge() + 1);
+//        knight.setAge(knight.getAge() + 1);
     }
 
-    public void setKnight(Knight knight) {
-        this.knight = knight;
+//    public void setKnight(Map<String, Knight> knights) {
+//        this.knights = knights;
+//    }
+
+    public void setKnights(List<Knight> knights) {
+        this.knights = knights;
     }
 
     @Override
     public String toString() {
-        return "W zawodach bierze udział rycerz :" + knight;
+        return "W zawodach bierą udział rycerze " + knights.stream().map(Objects::toString).collect(Collectors.joining(","));
     }
 }
